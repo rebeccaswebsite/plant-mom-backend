28f64fa1babb06c0d212e25368c2a2b4202f5aed
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-06-30 20:33:05 +0100

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 rebecca = User.create(username: "rebecca", password: "password")
 amy = User.create(username: "amy", password: "password")
-charlotte = User.create(username: "charlotte", password: "password")
-aydie = User.create(username: "aydie", password: "password")
+
+living_room = Room.create(user: rebecca, name: "living room")
+bedroom = Room.create(user: rebecca, name: "bedroom")
+kitchen = Room.create(user: amy, name: "kitchen")
+bathroom = Room.create(user: amy, name: "bathroom")
+
+succulent = Plant.create(common_name: "succulent")
+cactus = Plant.create(common_name: "cactus")
+aloe_vera = Plant.create(common_name: "aloe vera")
+spider_plant = Plant.create(common_name: "spider_plant")
+
+succulent_detail = Detail.create(plant: succulent, watering_schedule: "once a week", sunlight_exposure: "three hours of direct sun a day", temperature: 20, humidity: 40)
+cactus_detail = Detail.create(plant: cactus,  watering_schedule: "once a week", sunlight_exposure: "five hours of direct sun a day", temperature: 20, humidity: 40)
+aloe_vera_detail = Detail.create(plant: aloe_vera,  watering_schedule: "twice a week", sunlight_exposure: "four hours of direct sun a day", temperature: 15, humidity: 50)
+spider_plant_detail = Detail.create(plant: spider_plant,  watering_schedule: "once a week", sunlight_exposure: "two hours of direct sun a day", temperature: 10, humidity: 30 )
+
+plant_appearance_one = RoomPlant.create(room: living_room, plant: succulent)
+plant_appearance_two = RoomPlant.create(room: bedroom, plant: cactus)
+plant_appearance_three = RoomPlant.create(room: bathroom, plant: succulent)
+plant_appearance_four = RoomPlant.create(room: kitchen, plant: spider_plant)
+plant_appearance_five = RoomPlant.create(room: living_room, plant: aloe_vera)
+plant_appearance_six = RoomPlant.create(room: living_room, plant: spider_plant)
+plant_appearance_seven = RoomPlant.create(room: living_room, plant: succulent)
+plant_appearance_eight = RoomPlant.create(room: bedroom, plant: aloe_vera)
+plant_appearance_nine = RoomPlant.create(room: bedroom, plant: succulent)
