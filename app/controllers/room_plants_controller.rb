655453190f0a8c3aa63fef1e597d91745ece7001
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-07-08 14:18:54 +0100

    add create method for room_plants with route

--- app/controllers/room_plants_controller.rb
+++ app/controllers/room_plants_controller.rb
@@ -8,6 +8,19 @@ class RoomPlantsController < ApplicationController
         end
     end
 
+    def create
+        roomplant = RoomPlant.new(room_id: params[:room_id], plant_id: params[:plant_id])
+        
+        if roomplant.save
+            render json: roomplant
+        else
+          render json: {
+            error: "A plant/room instance was not created. Please try again later.",
+            status: 400
+            }, status: 400
+        end
+    end
+
     def destroy
         room_plant = RoomPlant.find_by(id: params[:id])
         room_plant.destroy
